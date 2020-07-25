fdc444dc0d3129bbca01aa44b159c8142e193d0a
taka603 <tkyxa603@gmail.com>
2020-07-25 10:42:53 +0900

    fmt

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -17,17 +17,17 @@
       @csrf
       @error('name')
       <tr><th>ERROR</th>
-      <td>{{$message}}/td></tr>
+      <td>{{$message}}</td></tr>
       @enderror
       <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
       @error('mail')
       <tr><th>ERROR</th>
-      <td>{{$message)}}</td></tr>
+      <td>{{$message}}</td></tr>
       @enderror
       <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
       @error('age')
       <tr><th>ERROR</th>
-      <td>{{$message}</td></tr>
+      <td>{{$message}}</td></tr>
       @enderror
       <tr><th>ERROR</th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
       <tr><th></th><td><input type="submit" value="send"></td></tr>
