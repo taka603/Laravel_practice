72993024501940dc939ce9f1547b64fae55dbebb
taka603 <tkyxa603@gmail.com>
2020-07-27 21:11:14 +0900

    edit

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -35,10 +35,16 @@ class HelloController extends Controller
             'name.required' => '名前は必ず入力して下さい。',
             'mail.email' => 'メールアドレスが必要です。',
             'age.numeric' => '年齢を整数で記入下さい。',
-            'age.between' => '年齢は0~150の間で入力ください。',
+            'age.min' => '年齢はゼロ以上で記入下さい。',
+            'age.max' => '年齢は200歳以下で記入して下さい。',
         ];
-        $validator = Validator::make($request->all(), $rules,
-                $messages);
+        $validator->sometimes('age', 'min:0', function($input){
+            return !is_int($input->age);
+        });
+        $validator->sometimes('age', 'max:200', function($input){
+            return !is_int($input->age);
+        });
+        
         if($validator->fails()) {
             return redirect('/hello')
                          ->withErrors($validator)
